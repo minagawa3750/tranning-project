887d3c08a3d4a6f094850099459a793ebadae7bf
kosuke <kosuke@KosukenoMacBook-Air.local>
2023-04-17 12:46:56 +0900

    contactテーブル、MVC作成(Http/Controllers/ContactContoroller.php、resources/views/contacts/index.blade.php、Models/Contact.php)

---
+++ src/database/migrations/2023_04_17_114908_create_contacts_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('contacts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name', 255);
+            $table->string('email', 255)->unique('email');
+            $table->text('content');
+            $table->integer('age');
+            $table->integer('gender');
+            $table->foreignId('department_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('contacts');
+    }
+}
