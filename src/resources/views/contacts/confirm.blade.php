fa32fd0a6e75dad5181e728431d73f1e00560dce
kosuke <kosuke@KosukenoMacBook-Air.local>
2023-04-18 10:06:27 +0900

    resources/views/contacts/confirm.blade.phpに年齢と性別入力フォーム実装

--- src/resources/views/contacts/confirm.blade.php
+++ src/resources/views/contacts/confirm.blade.php
@@ -14,15 +14,28 @@
         </div>
         <div class="form-group">
             <label for="name">お名前</label>
-            <input type="text" name="name">
+            <input type="text" name="name" id="name">
         </div>
         <div>
             <label for="email">メールアドレス</label>
-            <input type="email" name="email">
+            <input type="email" name="email" id="email">
+        </div>
+        <div>
+            <label for="age">年齢</label>
+            <input type="text" name="age" id="age">
+        </div>
+        <div>
+            <label for="gender">性別</label>
+            <select name="gender" id="gender">
+                <option value="">-- 選択してください --</option>
+                <option value="1">男性</option>
+                <option value="2">女性</option>
+                <option value="3">その他</option>
+            </select>
         </div>
         <div>
             <label for="content">お問い合わせ内容</label>
-            <textarea name="content"></textarea>
+            <textarea name="content" id="content"></textarea>
         </div>
     </div>
 </form>
